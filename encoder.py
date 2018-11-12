4e23364680714474c07e22e22e4baba07d659937
Cristyn Magnus <cmagnus@gmail.com>
2018-11-12 12:23:49 -0500

    Add random decode

--- encoder.py
+++ encoder.py
@@ -1,4 +1,12 @@
 # open unencoded.txt as variable f then close
+
+def myRandom(seed):
+    seed = 3.9 * seed * (1 - seed)
+    return (seed)
+
+def convertRandom(number):
+    return (int(256 * number))
+
 with open('unencoded.txt') as f:
     # read f and store contents in variable read_data
     read_data = f.read()
@@ -7,22 +15,31 @@ print(read_data)
 
 # make empty string
 encodedString = ""
+# set seed for random number sequence
+originalSeed = 0.25
+seed = originalSeed
 
 # loop through letters in read_data
 for letter in read_data:
     encodedLetter = ord(letter)
-    # add 13 to encodedLetter
-    encodedLetter += 13
+    seed = myRandom(seed)
+    offset = convertRandom(seed)
+    # add offset to encodedLetter
+    encodedLetter += offset
     # turn back into a letter
     encodedLetter = chr(encodedLetter)
     encodedString += encodedLetter
 print(encodedString)
 
 uncodedString = ""
- 
+
+seed = originalSeed 
 for letter in encodedString:
+
+    seed = myRandom(seed)
+    offset = convertRandom(seed)
     uncodedLetter = ord(letter)
-    uncodedLetter -= 13
+    uncodedLetter -= offset
     uncodedLetter = chr(uncodedLetter)
     uncodedString += uncodedLetter
 print(uncodedString)
