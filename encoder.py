75b5d8f8e22e34a8fd5fc413a76940b3ecb913b8
Cristyn Magnus <cmagnus@gmail.com>
2018-11-12 11:33:58 -0500

    Copy encoder from last week

---
+++ encoder.py
@@ -0,0 +1,14 @@
+with open('unencoded.txt') as f:# opens unencoded.txt as variable f then closes
+    read_data = f.read()	# read f and put it in variable read_data
+print(read_data)		# print contents of read_data (data from file)
+
+for thing in read_data:		# loop thru letters in read_data,letters->thing
+    print(thing)		# prints the thing variable 
+
+# c29 in-class activity: In c27-c129 repo:
+# 1. make text file
+# 2. read text file
+# 3. print text file
+#------------------------
+# You can spend the rest of the class period working on homework.
+
