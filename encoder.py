e5bb995a600b5575a135c0d3a986506e8ae05e26
Cristyn Magnus <cmagnus@gmail.com>
2018-11-12 11:38:47 -0500

    Reorganize comments for small screen

--- encoder.py
+++ encoder.py
@@ -1,14 +1,12 @@
-with open('unencoded.txt') as f:# opens unencoded.txt as variable f then closes
-    read_data = f.read()	# read f and put it in variable read_data
-print(read_data)		# print contents of read_data (data from file)
+# open unencoded.txt as variable f then close
+with open('unencoded.txt') as f:
+    # read f and store contents in variable read_data
+    read_data = f.read()
+# print contents of read_data
+print(read_data)
 
-for thing in read_data:		# loop thru letters in read_data,letters->thing
-    print(thing)		# prints the thing variable 
-
-# c29 in-class activity: In c27-c129 repo:
-# 1. make text file
-# 2. read text file
-# 3. print text file
-#------------------------
-# You can spend the rest of the class period working on homework.
+# loop through letters in read_data
+for letter in read_data:
+    # print the next letter
+    print(letter)
 
