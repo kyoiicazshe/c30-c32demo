9ce5374d541126b1ac685bc1a8f576f8aa32cabc
Cristyn Magnus <cmagnus@gmail.com>
2018-11-16 12:00:39 -0500

    Make all encoded chars letters

--- encoder.py
+++ encoder.py
@@ -5,7 +5,7 @@ def myRandom(seed):
     return (seed)
 
 def convertRandom(number):
-    return (int(256 * number))
+    return (int(26 * number))
 
 with open('unencoded.txt') as f:
     # read f and store contents in variable read_data
@@ -13,16 +13,26 @@ with open('unencoded.txt') as f:
 # print contents of read_data
 print(read_data)
 
+# make read_data lowercase
+#read_data = read_data.lower()
+#print(read_data)
+
 # make empty string
-encodedString = ""
-offset = 13	# for rot13
+encodedString = "" #offset = 13	# for rot13
+seed = 0.1
 
 # loop through letters in read_data
 for letter in read_data:
     if letter.isalpha():  # Only encode letters
         encodedLetter = ord(letter) # (a -> 97)
+        seed = myRandom(seed)
+        offset = convertRandom(seed)
+        #encodedLetter = encodedLetter - 97
+        encodedLetter -= 97
         # add offset to encodedLetter
         encodedLetter += offset # (97+13=110)
+        encodedLetter = encodedLetter % 26
+        encodedLetter += 97
         # turn back into a letter
         encodedLetter = chr(encodedLetter) # (110 -> n)
         encodedString += encodedLetter #(... -> ...n)
@@ -34,10 +44,10 @@ print(encodedString)
 uncodedString = ""
 
 #seed = originalSeed 
-for letter in encodedString:
+'''for letter in encodedString:
     if letter.isalpha():
         uncodedLetter = ord(letter)
         uncodedLetter -= offset
         uncodedLetter = chr(uncodedLetter)
     uncodedString += uncodedLetter
-print(uncodedString)
+print(uncodedString)'''
